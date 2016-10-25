903cdd1e982b7a643e04234418d045635e411cc6
Tim Walker <trwalker@godaddy.com>
2016-10-25 05:49:46 -0700

    Completed rendering and validation for stats

--- app/StatsService.js
+++ app/StatsService.js
@@ -1,19 +1,97 @@
+var container = null;
+var genderSelect = null;
+var weightInput = null;
+var goalSelect = null;
+
 class StatsService {
+
+    constructor() {
+    }
 
 	render(appContainer) {
         let row = document.createElement('div');
         row.className = 'row';
 
+        container = row;
+
         let column = document.createElement('div');
         column.className = 'col-xs-12';
 
-        renderWeightInput(column);
         renderGenderSelect(column);
+        renderWeightInput(column);
+        renderGoalSelect(column);
 
         row.appendChild(column);
         appContainer.appendChild(row);
 	}
 
+	getValues() {
+        let genderValue = genderSelect.value;
+        let weightValue = weightInput.value;
+        let goalValue = goalSelect.value;
+
+        let errors = [];
+
+        if(genderValue.length === 0) {
+            errors.push('Please select your gender');
+        }
+
+        let weight = parseInt(weightValue, 10);
+        if(weight !== weight) {
+            errors.push('Please enter a weight 1 through 999');
+        }
+
+        if(goalValue.length === 0) {
+            errors.push('Please select your goal');
+        }
+
+        return {
+            stats: {
+                gender: genderValue,
+                weight: weight,
+                goal: goalValue
+            },
+            errors: errors.length == 0 ? null : errors
+        };
+    }
+
+	hide() {
+        container.style.display = 'none';
+    }
+
+    show() {
+
+    }
+}
+
+function renderGenderSelect(column) {
+    let genderGroup = document.createElement('div');
+    genderGroup.className = 'form-group';
+
+    let genderSelect = document.createElement('select');
+    genderSelect.id = 'gender-select';
+    genderSelect.className = 'form-control';
+
+    genderSelect = genderSelect;
+
+    let options = [
+        { text: 'Select one...', value: '' },
+        { text: 'Male', value: 'male' },
+        { text: 'Female', value: 'female' },
+    ];
+
+    options.forEach((currentOption) => {
+        let option = document.createElement('option');
+        option.text = currentOption.text;
+        option.value = currentOption.value;
+
+        genderSelect.appendChild(option);
+    });
+
+    genderSelect.selectedIndex = 0;
+
+    genderGroup.appendChild(genderSelect);
+    column.appendChild(genderGroup);
 }
 
 function renderWeightInput(column) {
@@ -29,41 +107,41 @@ function renderWeightInput(column) {
     weightInput.maxLength = 3;
     weightInput.setAttribute('placeholder', 'Enter weight');
 
+    weightInput = weightInput;
+
     weightGroup.appendChild(weightInput);
     column.appendChild(weightGroup);
 }
 
-function renderGenderSelect(column) {
-    let genderGroup = document.createElement('div');
-    genderGroup.className = 'form-group';
+function renderGoalSelect(column) {
+    let goalGroup = document.createElement('div');
+    goalGroup.className = 'form-group';
 
-    let genderSelect = document.createElement('select');
-    genderSelect.id = 'weight-input';
-    genderSelect.className = 'form-control';
+    let goalSelect = document.createElement('select');
+    goalSelect.id = 'goal-select';
+    goalSelect.className = 'form-control';
 
-    let defaultOption = document.createElement('option');
-    defaultOption.text = 'Select one...';
-    defaultOption.value = '';
+    goalSelect = goalSelect;
 
-    let maleOption = document.createElement('option');
-    maleOption.text = 'Male';
-    maleOption.value = 'male';
+    let options = [
+        { text: 'Select one...', value: '' },
+        { text: 'Lose Weight', value: 'lose' },
+        { text: 'Gain Weight', value: 'gain' },
+    ];
 
-    let femaleOption = document.createElement('option');
-    femaleOption.text = 'Female';
-    femaleOption.value = 'female';
+    options.forEach((currentOption) => {
+        let option = document.createElement('option');
+        option.text = currentOption.text;
+        option.value = currentOption.value;
 
-    genderSelect.appendChild(defaultOption);
-    genderSelect.appendChild(maleOption);
-    genderSelect.appendChild(femaleOption);
+        goalSelect.appendChild(option);
+    });
 
-    genderSelect.selectedIndex = 0;
+    goalSelect.selectedIndex = 0;
 
-    genderGroup.appendChild(genderSelect);
-    column.appendChild(genderGroup);
+    goalGroup.appendChild(goalSelect);
+    column.appendChild(goalGroup);
 }
-
-
 
 let statsService = new StatsService();
 
