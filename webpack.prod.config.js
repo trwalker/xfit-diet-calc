7ea899c2e6da58eb35e1287db75168d8ddd44dd3
Tim Walker <trwalker@godaddy.com>
2016-10-31 06:39:38 -0700

    Fixing bad prod webpack config

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -2,7 +2,7 @@ var stripLoader = require('strip-loader');
 var defaultConfig = require('./webpack.config.js');
 
 var stripLoaderConfig = {
-	test: loader: stripLoader.loader('console.log', 'debugger'), [ /\.js$/, /\.es6$/ ], exclude: /node_modules/
+	loader: stripLoader.loader('console.log', 'debugger'), test: [ /\.js$/, /\.es6$/ ], exclude: /node_modules/
 };
 
 defaultConfig.module.loaders.push(stripLoaderConfig);
